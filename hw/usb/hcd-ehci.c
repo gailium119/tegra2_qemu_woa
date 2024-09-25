3aae42b71246057de53f5b85d65c33daa57fe5d5
gailium119 <77530553+gailium119@users.noreply.github.com>
2024-09-26 07:04:11 +0800

    Mod ehci to ignore failure

--- hw/usb/hcd-ehci.c
+++ hw/usb/hcd-ehci.c
@@ -1576,8 +1576,8 @@ static int ehci_state_fetchentry(EHCIState *ehci, int async)
 
     /* section 4.8, only QH in async schedule */
     if (async && (NLPTR_TYPE_GET(entry) != NLPTR_TYPE_QH)) {
-        fprintf(stderr, "non queue head request in async schedule\n");
-        return -1;
+        fprintf(stderr, "non queue head request in async schedule, ignored\n");
+        return 1;
     }
 
     switch (NLPTR_TYPE_GET(entry)) {
